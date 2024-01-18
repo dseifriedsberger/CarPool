453849b19f1ed5e4c1ff11f8a5a95e4d55392a56
d.seifriedsberger <d.seifriedsberger@htlvb.at>
2024-01-18 08:06:15 +0100

    Added SearchRide

--- CarPoolHTLVB/Components/RideStore.cs
+++ CarPoolHTLVB/Components/RideStore.cs
@@ -1,4 +1,5 @@
 ﻿using static CarPoolHTLVB.Components.Rides.OfferRide;
+using static CarPoolHTLVB.Components.Rides.SearchForRide;
 using MySqlConnector;
 using Dapper;
 using System.Configuration;
@@ -8,15 +9,37 @@ namespace CarPoolHTLVB.Components
 {
     
     public class RideStore
-    { 
+    {
+        public List<OfferRideModel> Rides = new();
         private string ConnString { get; set; }
         public RideStore(string connStrg)
         {
             ConnString = connStrg.Trim();
         }
-        public bool SaveRide(RideModel model)
+        private int GetLastRideID()
+        { 
+            using MySqlConnection connection = new(ConnString);
+            int id = -1;
+            try
+            {
+                connection.Open();
+                string sql = "SELECT Min(rideid) AS 'rideid' FROM rides";
+                MySqlCommand cmd = new(sql, connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    id = Convert.ToInt32(reader["rideid"]);
+                }
+                
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { connection.Close(); }
+            return id;
+        }
+        public bool SaveRide(OfferRideModel model)
         {
-            string newRideId = "newid";
+            int newRideId = GetLastRideID() + 10;
+            if(newRideId == 9) { newRideId = 10; }
             string sql = $"INSERT INTO rides VALUES('{newRideId}','{model.OffererID}', '{model.LocationFrom}', '{model.LocationTo}','{model.DepartureTime}','{model.ArrivalTime}','{model.VillagesPassed}', '{model.ClassmatesCanJoin}','{model.TeachersCanJoin}', {model.Seats}, '{model.Smoker}','{model.IsFree}','{model.Frequency}')";
          
             Console.WriteLine($"tried this cmd:{sql}");
@@ -25,6 +48,8 @@ namespace CarPoolHTLVB.Components
             {
                 TimeStamp = DateTime.UtcNow,
                 model.OffererID,
+                model.LocationFrom,
+                model.LocationTo,
                 model.DepartureTime,
                 model.ArrivalTime,
                 model.VillagesPassed,
@@ -49,6 +74,17 @@ namespace CarPoolHTLVB.Components
                 return false;
             }
             finally { connection.Close(); } 
+        }
+        public bool GetRides(SearchForRideModel model)
+        {
+            using MySqlConnection connection = new(ConnString);
+            string sql = "SELECT id, arrivaltime, ... FROM rides WHERE ... ORDER BY...";
+            try
+            {
+                Rides= connection.Query<OfferRideModel>(sql).ToList();
+                return true;
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); return false; }
         }
     }
 }
